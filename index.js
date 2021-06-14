5dcd9dce90a2c0af95192545ed507433dcea0586
Jin-Sih Lin <linpct@gmail.com>
2021-06-15 00:11:56 +0800

    force update

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-//var fromFile = require('./lib/fromFile');
-
 module.exports = {
     PlurkClient: require('./lib/PlurkClient'),
-    //fromFile: fromFile.fromFile,
-    //fromFileSync: fromFile.fromFileSync,
     limitedTo: require('./lib/limitedTo'),
     base36: require('./lib/base36'),
     urlMatch: require('./lib/urlMatch')
